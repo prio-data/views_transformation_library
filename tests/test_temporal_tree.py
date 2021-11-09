010854f9740566169c41ab621452e9bbdab2bcaf
peder2911 <pglandsverk@gmail.com>
2021-11-09 11:36:36 +0100

    Added mock test file

---
+++ tests/test_temporal_tree.py
@@ -0,0 +1,10 @@
+
+from unittest import TestCase
+from views_transformation_library import temporal_tree
+
+class TestTemporalTree(TestCase):
+    """
+    Tests for the temporal tree
+    """
+
+    pass # Delete this and write your tests below
